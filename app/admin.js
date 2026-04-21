0e0c46a2063f926e7cb1afdad1e59c6f4b17fee8
eduardorochael <eduardorochael@gmail.com>
2026-04-21 00:55:24 -0300

    Botões ligar e rota funcionando

--- app/admin.js
+++ app/admin.js
@@ -1,5 +1,5 @@
 // 🔥 Inicializa mapa
-let map = L.map('map').setView([-23.5505, -46.6333], 12);
+let map =
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
