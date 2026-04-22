c2dd5f111816c2c3c7bbaa5c7b76fe8853fe50fa
eduardorochael <eduardorochael@gmail.com>
2026-04-21 23:10:54 -0300

    banco postgres funcionando

--- app/database.py
+++ app/database.py
@@ -1,14 +1,27 @@
+import os
 from pathlib import Path
 
 from sqlalchemy import create_engine, inspect, text
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATABASE_URL = f"sqlite:///{BASE_DIR / 'test.db'}"
 
+# 🔥 PEGA DO RENDER
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# 🔥 FALLBACK (caso rode local sem variável)
+if not DATABASE_URL:
+    DATABASE_URL = f"sqlite:///{BASE_DIR / 'test.db'}"
+
+# 🔥 CORREÇÃO IMPORTANTE DO RENDER
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+
+# 🔥 ENGINE
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False},
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {},
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
